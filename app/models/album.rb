7a6c5772a08264f502b7f38f1f329f62c072147d
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-07-09 09:45:27 -0400

    Things uncommented out

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,15 +1,15 @@
-# class Album < ActiveRecord::Base
-#   validates :title, presence: true, uniqueness: { scope: :artist }
-#   validates :artist, presence: true
-#   validates :genre, presence: true
-#   validates :length, presence: true, numericality: { only_integer: true, greater_than: 0 }
-#
-#
-# private
-#   def create_album
-#     c = Albums.new
-#     c.valid?
-#     c.save
-#   end
-#
-# end
+class Album < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :artist }
+  validates :artist, presence: true
+  validates :genre, presence: true
+  validates :length, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+
+private
+  def create_album
+    c = Albums.new
+    c.valid?
+    c.save
+  end
+
+end
